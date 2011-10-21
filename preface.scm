cf8fad4f33711819f780025f957d181fc332d253
Mike De La Loza <mikedll@mikedll.com>
2011-10-21 12:55:33 -0700

    First commit.

---
+++ preface.scm
@@ -0,0 +1,22 @@
+
+
+I originally started this just as a way to give back to the world that is the family that was given to me, and to market my name and sort of create a persona for myself.
+
+
+
+A target audience would be:
+
+- Individual, curious people who love learning.
+
+- Young, teenage, independent-minded, self-sufficient individuals who are in Jr High or High School, know a little bit about programming but want to know more.
+
+- Very experienced programmers who get paid to write C# and Java (or Ruby on Rails!) have wanted to learn LISP, or go through SICP
+
+- Adults who have a lot of time on their hands to learn something new
+
+- Programmers of great experience who want to learn how to talk about what you already know.
+
+
+There is a slight bias toward males. Sorry.
+
+
